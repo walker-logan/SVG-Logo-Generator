b554d2849f836da06f2aba1319c0c7bba431a1e2
Walker Logan <walker.logan44@gmail.com>
2023-05-09 15:15:49 -0500

    debugging tests

--- tests/shapes.test.js
+++ tests/shapes.test.js
@@ -1,11 +1,11 @@
-const {Triangle, Square, Circle} = require("../lib/shapes.js")
+const { Triangle, Square, Circle } = require("../lib/shapes.js");
 
 // triangle test
 describe("Triangle test", () => {
   test("test for a triangle with a blue background", () => {
-    const shape = Triangle();
+    const shape = new Triangle();
     shape.setColor("blue");
-    expect(shape.render()).toEqual(
+    expect(shape.render().trim()).toEqual(
       '<polygon points="150, 18 244, 182 56, 182" fill="blue" />'
     );
   });
@@ -14,9 +14,9 @@ describe("Triangle test", () => {
 // square test
 describe("Square test", () => {
   test("test for a square with a purple background", () => {
-    const shape = Square();
+    const shape = new Square();
     shape.setColor("purple");
-    expect(shape.render()).toEqual(
+    expect(shape.render().trim()).toEqual(
       '<rect x="73" y="40" width="160" height="160" fill="purple" />'
     );
   });
@@ -25,9 +25,9 @@ describe("Square test", () => {
 // circle test
 describe("Circle test", () => {
   test("test for a circle with a #ca00ca background", () => {
-    const shape = Circle();
+    const shape = new Circle();
     shape.setColor("#ca00ca");
-    expect(shape.render()).toEqual(
+    expect(shape.render().trim()).toEqual(
       '<circle cx="150" cy="115" r="80" fill="#ca00ca" />'
     );
   });
